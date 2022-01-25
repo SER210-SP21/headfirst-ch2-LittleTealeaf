c23ad31559ca47c20a8f58cdfd45959fb15fb691
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-01-25 17:57:47 -0500

    Video 8 - Demo 3

--- app/src/main/java/edu/quinnipiac/ser210/CalcActivity.java
+++ app/src/main/java/edu/quinnipiac/ser210/CalcActivity.java
@@ -3,13 +3,61 @@ package edu.quinnipiac.ser210;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CalcActivity extends AppCompatActivity {
+
+    EditText input1, input2;
+    TextView res;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //Connects java to XML
         setContentView(R.layout.activity_main);
+
+        Button add = (Button) findViewById(R.id.buttonAddition);
+        Button subtract = (Button) findViewById(R.id.buttonSubtraction);
+        input1 = (EditText) findViewById(R.id.input1);
+        input2 = (EditText) findViewById(R.id.input2);
+        res = (TextView) findViewById(R.id.result);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                performOp('+');
+            }
+        });
+
+        subtract.setOnClickListener(v -> performOp('-'));
+
+
+    }
+
+    private void performOp(char operation) {
+        if(input1.getText() != null && input2.getText() != null) {
+            double num1 = Double.parseDouble(input1.getText().toString());
+            double num2 = Double.parseDouble(input2.getText().toString());
+
+            double result = 0;
+
+            switch(operation) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    break;
+            }
+            res.setText(Double.toString(result));
+        }
     }
 }
